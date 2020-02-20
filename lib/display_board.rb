681939e9a6689702cb990c88b8548c18595eea31
Zeynab Jibreel <zeina2025@gmail.com>
2020-02-20 19:59:26 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,12 @@
  # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
 
-  |   |
- -----------
-    |   |
- -----------
-    |   |
-end
+
+    puts "   |   |   "
+    puts "-----------"
+    puts "   |   |   "
+    puts "-----------"
+    puts "   |   |   "   
+  end
+  puts " Welcome to Tic Tac Toe!"
+    
